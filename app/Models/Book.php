d5c1dc4f3de6a71af29cdfd250fc0b3542724d1d
nahid <nahidkahn9144@gmail.com>
2024-09-18 21:22:06 +0530

    successfull

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -9,10 +9,8 @@ class Book extends Model
 {
     use HasFactory;
     public $timestamps = false;
-     // Specify the table associated with the model, if different from the default
      protected $table = 'books';
 
-     // Define the fillable attributes for mass assignment
      protected $fillable = [
          'title',
          'author',
@@ -20,13 +18,12 @@ class Book extends Model
          'published_date',
      ];
  
-     // Optionally, you can define any hidden or cast attributes here
      protected $hidden = [
-         // attributes to hide
+
      ];
  
      protected $casts = [
-         'published_date' => 'datetime',
+        //  'published_date' => 'datetime',
      ];
      
 }
--- database/migrations/2024_09_11_145923_create_book_table.php
+++ database/migrations/2024_09_11_145923_create_book_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
-            $table->string('title')->unique();
+            $table->string('title'); 
             $table->string('author');
             $table->string('description');
             $table->date('published_date'); 
