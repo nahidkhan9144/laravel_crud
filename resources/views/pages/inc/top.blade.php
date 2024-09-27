fab3bd9d7194c75d245d6072b089706a9ad07268
nahid <nahidkahn9144@gmail.com>
2024-09-27 21:53:27 +0530

    add css file

--- resources/views/pages/inc/top.blade.php
+++ resources/views/pages/inc/top.blade.php
@@ -1,3 +1,5 @@
-<div class="d-flex bg-secondary bg-gradient">
-    <p><a href="/goToHome" class="mx-3"> Dashboard </a> > <a href="/table" class="mx-3"> Books Data </a> > <a href="/logout" class="mx-3"> Logout </a></p>
+<link href="{{ URL::asset('css/top.css') }}" rel="stylesheet" type="text/css" >
+    
+<div class="d-flex bg-gradient" style="background-color: rgb(208 218 227)">
+    <p><a href="/goToHome" id="bg" class="mx-3"> Dashboard </a>><a href="/table" id="bg" class="mx-3"> Books Data </a>><a href="/logout" id="bg" class="mx-3"> Logout </a></p>
 </div>
