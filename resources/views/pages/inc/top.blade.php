c637c69700d8fe0b22e5d6032113280e14388959
nahid <nahidkahn9144@gmail.com>
2024-09-27 22:03:53 +0530

    css

--- resources/views/pages/inc/top.blade.php
+++ resources/views/pages/inc/top.blade.php
@@ -1,5 +1,5 @@
 <link href="{{ URL::asset('css/top.css') }}" rel="stylesheet" type="text/css" >
     
 <div class="d-flex bg-gradient" style="background-color: rgb(208 218 227)">
-    <p><a href="/goToHome" id="bg" class="mx-3"> Dashboard </a>><a href="/table" id="bg" class="mx-3"> Books Data </a>><a href="/logout" id="bg" class="mx-3"> Logout </a></p>
+    <p><a href="/goToHome" id="bg" class="mx-3"> <i class="fa fa-home"></i> Dashboard </a>><a href="/table" id="bg" class="mx-3"><i class="fa fa-book"></i> Books Data </a>><a href="/logout" id="bg" class="mx-3"><i class="fa fa-sign-out"></i> Logout </a></p>
 </div>
